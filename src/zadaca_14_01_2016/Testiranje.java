14528d73adf6cb66a53f1519f1d7e04e84fb708c
Argamore <sefer.kuduzovic@gmail.com>
2016-01-14 22:00:48 +0100

    Testiranje 2

--- src/zadaca_14_01_2016/Testiranje.java
+++ src/zadaca_14_01_2016/Testiranje.java
@@ -3,7 +3,7 @@ package zadaca_14_01_2016;
 public class Testiranje {
 
 	public static void main(String[] args) {
-		System.out.println("Test 1, 2,3");
+		System.out.println("Test 1, 2, 3");
 
 	}
 
